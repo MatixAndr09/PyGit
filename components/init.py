528c9541280d4f089703e089d96122735272f0e5
Mathew <u116_matand_waw@technischools.com>
2024-10-27 18:18:18 +0100

    Writen init.py file for initailizng the program
    For windows winget is required. If you are running it for the first time run it as administrator in order to check for git if git is not installed it will isntall it

--- components/init.py
+++ components/init.py
@@ -1,5 +1,53 @@
 import subprocess
+import platform
+import os
 
 
 def init():
-    print("Hi :)")
+    print("[*]  This is init script please wait...")
+    check = subprocess.run("git --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    os_name = platform.system()
+    if check.returncode != 0:
+        print("[-]  Git is not installed...")
+        if os_name == "Windows":
+            print("[*]  Installing git...")
+            subprocess.run(f"winget install --id Git.Git -e --source winget", shell=True, stdout=subprocess.PIPE)
+        elif os_name == "Linux":
+            print("[*]  Installing git...")
+            subprocess.run("sudo apt-get update", shell=True)
+            subprocess.run("sudo apt-get install git", shell=True)
+        elif os_name == "Darwin":
+            print("[*]  Installing git...")
+            subprocess.run("brew install git", shell=True)
+    else:
+        print(f"[+]  Git is installed (version: {check.stdout.decode('utf-8').strip()})")
+
+    print("[*]  Checking python and libs...")
+
+    pyver = platform.python_version()
+    print(f"[+]  Python version: {pyver}")
+    if pyver[0] != "3":
+        print("[-]  Python version is not 3.x.x")
+        print("[-]  Please install python 3.x.x")
+        exit(1)
+
+    libs = ["rich", "textual", "requests"]
+    for lib in libs:
+        check = subprocess.run(f"pip show {lib}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if check.returncode != 0:
+            print(f"[-]  {lib} is not installed...")
+            print(f"[*]  Installing {lib}...")
+            subprocess.run(f"pip install {lib}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            version_line = next(
+                line for line in check.stdout.decode('utf-8').split('\n') if line.startswith("Version:")
+            )
+            version = version_line.split(":")[1].strip()
+            print(f"[+]  {lib} is installed (version: {version})")
+
+    print("[+]  Init script finished...")
+    print("[*]  Starting the application & clearing terminal...")
+    if os_name == "Windows":
+        os.system("cls")
+    elif os_name in ["Linux", "Darwin"]:
+        os.system("clear")
