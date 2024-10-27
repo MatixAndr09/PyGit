3b9831bab10eca1e4d511f58b1d05c2d50d962da
Mathew <u116_matand_waw@technischools.com>
2024-10-27 22:05:41 +0100

    git functionality

--- components/GIT_CLIENT.py
+++ components/GIT_CLIENT.py
@@ -1,41 +1,44 @@
+import subprocess
+
+
 def gitINIT():
     """Initialize a new GitHub repo in the current directory"""
-    pass
+    subprocess.run(["git", "init"], check=True)
 
 
 def gitCLONE(url):
-    """Clone a GitHub public repo in to current directory"""
-    pass
+    """Clone a GitHub public repo into the current directory"""
+    subprocess.run(["git", "clone", url], check=True)
 
 
 def gitADD(filepath):
     """Adds files to the staging area. If you use the '.' argument, it will add all files in the current directory"""
-    pass
+    subprocess.run(["git", "add", filepath], check=True)
 
 
 def gitCOMMIT(message):
     """Commits the files in the staging area with the message"""
-    pass
+    subprocess.run(["git", "commit", "-m", message], check=True)
 
 
 def gitSTATUS():
-    """Shows the status of the current repo. (Modified, deleted,created, files)"""
-    pass
+    """Shows the status of the current repo. (Modified, deleted, created, files)"""
+    subprocess.run(["git", "status"], check=True)
 
 
 def gitPULL():
     """Pulls the latest changes from the remote repo"""
-    pass
+    subprocess.run(["git", "pull"], check=True)
 
 
 def gitPUSH():
     """Pushes the changes to the remote repo"""
-    pass
+    subprocess.run(["git", "push"], check=True)
 
 
 def gitBRANCH(branch):
     """Creates a new branch or change to other branch"""
-    pass
+    subprocess.run(["git", "branch", branch], check=True)
 
 
 def gitCHECKOUT(branch):
@@ -50,4 +53,4 @@ def gitMERGE(branch):
 
 def gitLOG():
     """Shows the commit log"""
-    pass
+    subprocess.run(["git", "log"], check=True)
