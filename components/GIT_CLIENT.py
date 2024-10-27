16e44e260f390957ed1da48a62009af9de667855
Mathew <u116_matand_waw@technischools.com>
2024-10-27 21:46:07 +0100

    Writen the defs for GIT_CLIENT.py

---
+++ components/GIT_CLIENT.py
@@ -0,0 +1,53 @@
+def gitINIT():
+    """Initialize a new GitHub repo in the current directory"""
+    pass
+
+
+def gitCLONE(url):
+    """Clone a GitHub public repo in to current directory"""
+    pass
+
+
+def gitADD(filepath):
+    """Adds files to the staging area. If you use the '.' argument, it will add all files in the current directory"""
+    pass
+
+
+def gitCOMMIT(message):
+    """Commits the files in the staging area with the message"""
+    pass
+
+
+def gitSTATUS():
+    """Shows the status of the current repo. (Modified, deleted,created, files)"""
+    pass
+
+
+def gitPULL():
+    """Pulls the latest changes from the remote repo"""
+    pass
+
+
+def gitPUSH():
+    """Pushes the changes to the remote repo"""
+    pass
+
+
+def gitBRANCH(branch):
+    """Creates a new branch or change to other branch"""
+    pass
+
+
+def gitCHECKOUT(branch):
+    """[NOT AVAILABLE]"""
+    pass
+
+
+def gitMERGE(branch):
+    """[NOT AVAILABLE]"""
+    pass
+
+
+def gitLOG():
+    """Shows the commit log"""
+    pass
